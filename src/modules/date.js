aee705968613d4ef40fbe578ec073b194f4d41d4
joonhyeok han <virq@naver.com>
2024-02-11 20:23:04 +0900

    ✨ feat: Convert UTC timezone into KST

--- src/modules/date.js
+++ src/modules/date.js
@@ -1,9 +1,10 @@
-import { format, setDefaultOptions } from "date-fns";
+import { setDefaultOptions } from "date-fns";
 import { ko } from "date-fns/locale";
+import { format } from "date-fns-tz";
 
 setDefaultOptions({ locale: ko });
 
 export const getFormatTodayDate = (formatStr) => {
   const utc = new Date();
-  return format(utc, formatStr);
+  return format(utc, formatStr, { timeZone: "Asia/Seoul" });
 };
