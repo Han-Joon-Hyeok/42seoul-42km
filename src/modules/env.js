d084822773cb2df7c67f90fa05f972cea7103bfb
joonhan <joonhan@student.42seoul.kr>
2024-08-28 12:01:54 +0900

    ✨ feat: Change issue creator to GitHub App bot instead of GitHub account

--- src/modules/env.js
+++ src/modules/env.js
@@ -5,6 +5,9 @@ const env = {
     owner: process.env.OWNER,
     repo: process.env.REPO,
     auth: process.env.TOKEN,
+    appId: process.env.GITHUB_APP_ID,
+    installationId: process.env.GITHUB_APP_INSTALLATION_ID,
+    privateKey: process.env.GITHUB_APP_PRIVATE_KEY,
   },
   openData: {
     key: process.env.OPEN_DATA_SERVICE_KEY,
--- src/modules/github.js
+++ src/modules/github.js
@@ -1,15 +1,9 @@
-import { Octokit } from "octokit";
+import { Octokit } from "@octokit/rest";
+import { createAppAuth } from "@octokit/auth-app";
 
 import env from "./env.js";
 import { getFormatTodayDate } from "./date.js";
 import logger from "./logger.js";
-
-const octokit = new Octokit({
-  auth: env.github.auth,
-});
-
-const OWNER = env.github.owner;
-const REPO = env.github.repo;
 
 const todayDate = getFormatTodayDate("YYYY년 MM월 DD일");
 const year = getFormatTodayDate("YYYY년");
@@ -17,19 +11,23 @@ const month = getFormatTodayDate("MM월");
 
 export const createGithubIssue = async (nofiticationMessage) => {
   try {
-    const response = await octokit.request(
-      `POST /repos/${OWNER}/${REPO}/issues`,
-      {
-        owner: "OWNER",
-        repo: "REPO",
-        title: `${todayDate} 날씨 정보`,
-        body: nofiticationMessage,
-        labels: [`${year} ${month}`],
-        headers: {
-          "X-GitHub-Api-Version": "2022-11-28",
-        },
-      }
-    );
+    const { owner, repo } = env.github;
+    const appOctokit = new Octokit({
+      authStrategy: createAppAuth,
+      auth: {
+        appId: env.github.appId,
+        privateKey: env.github.privateKey,
+        installationId: env.github.installationId,
+      },
+    });
+
+    const response = await appOctokit.issues.create({
+      owner,
+      repo,
+      title: `${todayDate} 날씨 정보`,
+      body: nofiticationMessage,
+      labels: [`${year} ${month}`],
+    });
     logger.info("GitHub 이슈를 성공적으로 생성했습니다.");
   } catch (error) {
     logger.error(error);
