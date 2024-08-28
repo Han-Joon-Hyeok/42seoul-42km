ad21c510ce62b5d92521873dcb0d766de6632e4a
joonhan <joonhan@student.42seoul.kr>
2024-08-28 12:06:19 +0900

    ✏️ fix: Remove GITHUB_ prefix in env

--- src/modules/env.js
+++ src/modules/env.js
@@ -5,9 +5,9 @@ const env = {
     owner: process.env.OWNER,
     repo: process.env.REPO,
     auth: process.env.TOKEN,
-    appId: process.env.GITHUB_APP_ID,
-    installationId: process.env.GITHUB_APP_INSTALLATION_ID,
-    privateKey: process.env.GITHUB_APP_PRIVATE_KEY,
+    appId: process.env.APP_ID,
+    installationId: process.env.APP_INSTALLATION_ID,
+    privateKey: process.env.APP_PRIVATE_KEY,
   },
   openData: {
     key: process.env.OPEN_DATA_SERVICE_KEY,
